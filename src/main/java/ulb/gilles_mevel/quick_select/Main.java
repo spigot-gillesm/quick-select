fe42c2580f6311c7ad138716e533355f398d0f7f
gilles <gilles1348@gmail.com>
2023-10-17 15:54:46 +0200

    Fixed issue and added javadocs

--- src/main/java/ulb/gilles_mevel/quick_select/Main.java
+++ src/main/java/ulb/gilles_mevel/quick_select/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(final String[] args) {
 		final Integer[] numbers = {0, 8, 4, 15, 14, -4, 0, 78, -25, 14, 75, 2};
 		final var quickSelect = new QuickSelect<>(numbers);
-		System.out.println("k-th element: " + quickSelect.findKElement(0));
+		System.out.println("k-th element: " + quickSelect.findKElement(11, true));
 	}
 
 }
--- src/main/java/ulb/gilles_mevel/quick_select/QuickSelect.java
+++ src/main/java/ulb/gilles_mevel/quick_select/QuickSelect.java
@@ -13,19 +13,39 @@ public class QuickSelect<T> {
 		this.elements = elements;
 	}
 
-	@SuppressWarnings("unchecked")
+	/**
+	 * Find the k-th smallest (or greatest) element, depending on findGreatest value.
+	 *
+	 * @param findGreatest whether this instance should look for the greatest k-th element instead of smallest
+	 * @return the value of the k-th smallest (or greatest) element
+	 */
+	public T findKElement(final int k, final boolean findGreatest) {
+		int effectiveK = k;
+
+		//Asking for the greatest element is equivalent to asking for "the least great" element
+		if(findGreatest) {
+			effectiveK = elements.length - k - 1;
+		}
+
+		return findKElement(effectiveK);
+	}
+
+	/**
+	 * Find the k-th smallest element in this instance's array.
+	 *
+	 * @return the value of the k-th smallest element
+	 */
 	public T findKElement(final int k) {
-		if(elements.length < 1) {
+		if (elements.length < 1) {
 			throw new IllegalArgumentException("There must be at least one element in the array");
 		}
-		if(elements.length == 1) {
-			return (T) elements[0];
+		if (elements.length == 1) {
+			return get(0);
 		}
 
 		return findKElement(k, 0, elements.length);
 	}
 
-	@SuppressWarnings("unchecked")
 	private T findKElement(final int k, final int left, final int right) {
 		int upperBound = right;
 		int lowerBound = left;
@@ -33,13 +53,14 @@ public class QuickSelect<T> {
 		while(upperBound != lowerBound) {
 			int pivotIndex = random.nextInt(upperBound - lowerBound) + lowerBound;
 			pivotIndex = partition(lowerBound, upperBound, pivotIndex);
-			System.out.println("pivot: " + pivotIndex);
-			System.out.println("pivot value: " + elements[pivotIndex]);
-			System.out.println("left: " + lowerBound);
-			System.out.println("right: " + upperBound);
 
+			//Should the partition be made of only equivalent values, return said value
+			if((upperBound - lowerBound <= 2) && elements[lowerBound].equals(elements[upperBound-1])) {
+				return get(lowerBound);
+			}
+			//If the found pivot is == to k, the the right value was found
 			if(pivotIndex == k) {
-				return (T) elements[pivotIndex];
+				return get(pivotIndex);
 			//If k is smaller than the pivot index, the value the algorithm is looking for is on the left of the pivot index
 			} else if(k < pivotIndex) {
 				upperBound = pivotIndex;
@@ -49,20 +70,33 @@ public class QuickSelect<T> {
 			}
 		}
 
-		return (T) elements[left];
+		return get(left);
 	}
 
-	@SuppressWarnings("unchecked")
+	/**
+	 * Partition the values between left and right indexes where:
+	 * <ul>
+	 *   <li>all elements between left and the value at the pivot index are smaller or equal than said value</li>
+	 *   <li>all elements between the value at the pivot index and right are greater or equal than said value</li>
+	 * </ul>
+	 * The method guarantees that the position of the value at the given pivot index will be at its right position
+	 * in the array, i.e. if the array was completely sorted, that value would be at the same index.
+	 *
+	 * @param left the left index, i.e. lower bound
+	 * @param right the right index, i.e. upper bound
+	 * @param pivotIndex the index at which the value will be used as a pivot
+	 * @return the final position/index of the pivot value
+	 */
 	private int partition(final int left, final int right, final int pivotIndex) {
 		//Get value at pivotIndex
-		final var pivotValue = elements[pivotIndex];
+		final T pivotValue = get(pivotIndex);
 		//Push the pivotValue to the end of the partition
 		swap(pivotIndex, right-1);
 
 		int currentIndex = left;
 
 		for(int i = left; i < right; i++) {
-			if(elements[i].compareTo((T) pivotValue) < 0) {
+			if(elements[i].compareTo(pivotValue) < 0) {
 				swap(i, currentIndex);
 				currentIndex++;
 			}
@@ -74,17 +108,19 @@ public class QuickSelect<T> {
 		return currentIndex;
 	}
 
+	/**
+	 * Swap the elements of the array
+	 */
 	private void swap(final int i, final int j) {
 		final var tempI = elements[i];
 		elements[i] = elements[j];
 		elements[j] = tempI;
 	}
 
-	private enum Mode {
-
-		GREATEST,
-		SMALLEST
-
+	//Method used to avoid unchecked in the rest of the code
+	@SuppressWarnings("unchecked")
+	private T get(final int i) {
+		return (T) elements[i];
 	}
 
 }
